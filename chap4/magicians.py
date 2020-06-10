a8709d546fd3521c9cff0560ee5ada91a242a3bd
priscalefawane <62431006+priscalefawane@users.noreply.github.com>
2020-06-10 13:16:57 +0200

    my-pizzas

--- chap4/magicians.py
+++ chap4/magicians.py
@@ -14,3 +14,10 @@ for magician in magicians:
     print(f"I can't wait to see your next trick, {magician.title()}.\n")
 
     print(f"Thank you, everyone. That was a great magic show!")
+
+
+ # what happens when you forgot the colon (syntax error)
+
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians
+    print(magician)
---
+++ chap4/tiy105_4-1.py
@@ -0,0 +1,5 @@
+# My favorite pizzas
+pizzas = ['chicken-pine', 'pepperroni', 'supreme',]
+for pizza in pizzas:
+    print(f"I like {pizza} pizza!")
+print(f"\nI like chicken-pie with lots of pines on it.\nDouble chesse is always the way on pepperoni.\nWow!!! The sauces on supreme, so delicious!\n\nI really like pizza! ")
