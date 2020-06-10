87c69c7fed056f09be716e0e41b1240cc2f7efb0
priscalefawane <62431006+priscalefawane@users.noreply.github.com>
2020-06-10 14:11:16 +0200

    my-carnivores

---
+++ chap4/tiy105_4-2.py
@@ -0,0 +1,6 @@
+animals = ['leopard', 'lion', 'cheetah']
+for animal in animals:
+    print(animal)
+    print(f"A {animal} is one of carnivorous animals.\n")
+
+print("Any of these animals is a meat eating animal.")
