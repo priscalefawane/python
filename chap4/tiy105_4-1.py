605e7312721fe7be969bebeed904ae98809682ac
priscalefawane <62431006+priscalefawane@users.noreply.github.com>
2020-06-11 20:58:31 +0200

    my_pizzas

--- chap4/tiy105_4-1.py
+++ chap4/tiy105_4-1.py
@@ -1,5 +1,9 @@
 # My favorite pizzas
-pizzas = ['chicken-pine', 'pepperroni', 'supreme',]
+pizzas = ['chicken & pine', 'pepperoni', 'supreme']
 for pizza in pizzas:
     print(f"I like {pizza} pizza!")
-print(f"\nI like chicken-pie with lots of pines on it.\nDouble chesse is always the way on pepperoni.\nWow!!! The sauces on supreme, so delicious!\n\nI really like pizza! ")
+
+print(f"\nI like chicken & pine with lots of pines on it."
+      f"\nDouble chesse is always the way on pepperoni."
+      f"\nWow!!! The sauces on supreme, so delicious!"
+      f"\n\nI really like pizza! ")
